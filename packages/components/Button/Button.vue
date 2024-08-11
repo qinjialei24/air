e83e946e6a30fa8e4c79c8bad86fc8d154b90063
qinjialei24 <954551222@qq.com>
2024-08-11 20:46:22 +0800

    refactor(monorepo): update package.json and add utils export
    - Remove the scoped '@air' from the name in the root package.json.
    - Add workspace:* dependencies for '@air/utils' and '@air/components' in the root package.
    - Change the main entry point in packages/components/package.json from index.js to index.ts.
    - Add an export statement in packages/utils/index.ts to re-export './install'.
    
    This commit adjusts package configurations and extends the utility exports to support
    a monorepo structure more effectively.

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'AirButton',
+})
+</script>
+<template>
+  <button  class="air-button">
+    button
+  </button>
+</template>
---
+++ packages/components/Button/index.ts
@@ -0,0 +1,4 @@
+import { withInstall } from "@air/utils";
+import Button from "./Button.vue";
+
+export const AirButton = withInstall(Button);
